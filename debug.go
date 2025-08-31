0f7da22f244ee3a0f6a41dae236c017a3352ccc9
MR-Valdez <khadwiz@gmail.com>
2025-08-31 15:40:39 -0500

    additionaldebuging added

--- debug.go
+++ debug.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"github.com/go-vgo/robotgo"
+	"github.com/lxn/win"
 )
 
 func logDebug(msg string, args ...any) {
@@ -36,12 +37,17 @@ func printDebugInfo() {
 	bitmap := robotgo.CaptureScreen(mouseX, mouseY, 1, 1)
 	defer robotgo.FreeBitmap(bitmap)
 
-	hexString := robotgo.GetPixelColor(mouseX, mouseY)
+	hwnd := win.GetForegroundWindow()
+	if hwnd == 0 {
+		fmt.Println("No foreground window")
+		return
+	}
+	dpi := win.GetDpiForWindow(hwnd)
 
 	fmt.Println("------ DEBUG INFO ------")
 	fmt.Printf("Mouse: (%d, %d)\n", mouseX, mouseY)
 	fmt.Printf("Window: origin=(%d, %d), size=(%d x %d)\n", winX, winY, winW, winH)
 	fmt.Printf("Mouse relative to window: (%d, %d)\n", mouseX-winX, mouseY-winY)
-	fmt.Printf("Pixel color: #%s \n", hexString) //(R:%d G:%d B:%d)
+	fmt.Printf("DPI for window: %d (100%% scaling = 96 DPI)\n", dpi)
 	fmt.Println("-------------------------")
 }
